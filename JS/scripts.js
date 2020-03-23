df91eebd74fb7a756188614c95293d6b599febad
Taylor Phillips <taylorphillips133@gmail.com>
2020-03-23 15:18:03 -0700

    add Multiply, Subtract,Divide functions

--- JS/scripts.js
+++ JS/scripts.js
@@ -13,5 +13,5 @@ var divide = function (number1, number2) {
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2);
+var result = multiply(number1, number2);
 alert(result);

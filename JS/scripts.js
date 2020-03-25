a7a9ef2a933a0cde5c282d045dfc40106bbda0dc
Taylor Phillips <taylorphillips133@gmail.com>
2020-03-25 15:28:26 -0700

    add new html and js steps to building calculator
    Co-Authored: Maria Laguneros <maria58@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,17 +1,20 @@
-var add = function (number1, number2) {
+var add = function(number1, number2) {
   return number1 + number2;
 };
-var subtract = function (number1, number2) {
-  return number1 - number2
-}
-var multiply = function (number1, number2) {
-  return number1 * number2
-}
-var divide = function (number1, number2) {
-  return number1 / number2
-}
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2);
-alert(result);
+
+alert(add(number1, number2));

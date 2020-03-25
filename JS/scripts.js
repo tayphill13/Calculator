96c7840523fbcf3027aa15daaafa941c7688bbb3
Taylor Phillips <taylorphillips133@gmail.com>
2020-03-25 16:03:57 -0700

    add parseint to make user input string to a number

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,5 @@
-var add = function(number1, number2) {
+ //Business (or back-end) logic:
+ var add = function(number1, number2) {
   return number1 + number2;
 };
 
@@ -14,7 +15,12 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+// Everything below this line isuser interface (or front-end) logic:
 
-alert(add(number1, number2));
+$(document).ready(function() {
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+
+  alert(add(number1, number2));
+
+});

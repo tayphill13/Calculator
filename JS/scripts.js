463bd40e885d20748a72e3f75d64f6e8f2c509d1
Taylor Phillips <taylorphillips133@gmail.com>
2020-03-25 16:23:38 -0700

    add a submit function and debugged js

--- JS/scripts.js
+++ JS/scripts.js
@@ -18,9 +18,12 @@ var divide = function(number1, number2) {
 // Everything below this line isuser interface (or front-end) logic:
 
 $(document).ready(function() {
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
 
-  alert(add(number1, number2));
+    alert(add(number1, number2));
 
+  });
 });
